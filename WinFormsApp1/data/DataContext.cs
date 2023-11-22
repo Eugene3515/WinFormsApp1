8345f6497d9c581e627b18ad80304b8aed7a3556
Eugene1315 <hibin.eugene@yandex.ru>
2023-11-22 15:09:02 +0300

    Добавьте файлы проекта.

---
+++ WinFormsApp1/data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1.data
+{
+    internal class DataContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=agency;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ WinFormsApp1/data/WorkList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1.data
+{
+    internal class WorkList
+    {
+        public int Id { get; set; }
+        public string Company { get; set; }
+        public int Phone { get; set; }
+
+    }
+}
